18e25d5e012a80930a9ca39d2eef504763b0c78e
danielglazer26 <danielglazer200026@gmail.com>
2022-04-06 21:05:08 +0200

    Standard SPI project

--- lab05/source/ServiceProvider/client/src/main/java/glazer/daniel/pwr/app/gui/MainWindow.java
+++ lab05/source/ServiceProvider/client/src/main/java/glazer/daniel/pwr/app/gui/MainWindow.java
@@ -8,22 +8,21 @@ import glazer.daniel.pwr.app.api.DataSet;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.ServiceLoader;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class MainWindow extends JFrame {
 
-    private DefaultListModel<String> listModelX;
-    private DefaultListModel<String> listModelY;
+    private DefaultListModel<String> listModelX, listModelY, listModelResults;
     private ServiceLoader<AnalysisService> loader;
-    private DataSet dataContainer = null;
+    private DataSet dataContainer;
     private JPanel panel1;
     private JList<String> listX;
     private JList<String> listY;
@@ -33,62 +32,97 @@ public class MainWindow extends JFrame {
     private JLabel label2;
     private JTextField optionsField;
     private JButton algorithmStartButton;
+    private JList<String> list1;
     private String pathToFile;
 
     public MainWindow() {
         setContentPane(panel1);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        dataContainer = new DataSet();
 
         loadFileButton.addActionListener(e -> {
             loader = ServiceLoader.load(AnalysisService.class);
             pathToFile = actionChooseFile();
             loadDataFromFile();
-            loader.stream().forEach(analysisServiceProvider -> {
-
-                if (analysisServiceProvider.get().getName().equals("This class calculate Mediana")) {
-                    try {
-                        System.out.println(analysisServiceProvider.get().getName());
-                        var a = analysisServiceProvider.get();
-                        a.submit(dataContainer);
-                        Arrays.stream(a.retrieve(true).getData()).forEach(strings -> Arrays.stream(strings).forEach(System.out::println));
-                    } catch (AnalysisException | ClusteringException ex) {
-
-                    }
-                }
-            });
-
-
         });
 
         algorithmStartButton.addActionListener(e -> {
-            AnalysisService as = null;
-            var it = loader.iterator();
-            for (int i = 0; i <= listY.getSelectedIndex(); i++) {
-                as = it.next();
+            AnalysisService as = getAnalysisService();
+            if (as != null)
+                getDataFromService(as);
+            pack();
+        });
+
+        list1.addComponentListener(new ComponentListener() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                pack();
             }
 
-            try {
-                as.submit(dataContainer);
+            @Override
+            public void componentMoved(ComponentEvent e) {
 
-                String opt = optionsField.getText();
-                if (opt.length() > 0) {
-                    as.setOptions(opt.split(","));
-                }
-               else {
-                    as.setOptions(new String[]{});
-                }
+            }
 
-               DataSet dataSet = as.retrieve(true);
-                setLists(dataSet.getData());
-            } catch (AnalysisException | NullPointerException | ClusteringException ex) {
-                ex.printStackTrace();
+            @Override
+            public void componentShown(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+
             }
         });
+
         pack();
 
+    }
 
-        dataContainer = new DataSet();
+    private void getDataFromService(AnalysisService as) {
+        try {
+            checkOptions(as);
+            as.submit(dataContainer);
+
+            AtomicReference<DataSet> dataSet = new AtomicReference<>(new DataSet());
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        DataSet ds = as.retrieve(true);
+                        if (ds != null) {
+                            dataSet.set(ds);
+                            setListResults(dataSet.get().getData());
+                            break;
+                        }
+                        Thread.sleep(100);
+                    } catch (ClusteringException | InterruptedException ex) {
+
+                    }
+                }
+            }).start();
+
+        } catch (AnalysisException | NullPointerException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void checkOptions(AnalysisService as) throws AnalysisException {
+        String opt = optionsField.getText();
+        if (opt.length() > 0) {
+            as.setOptions(opt.split(","));
+        } else {
+            as.setOptions(new String[]{});
+        }
+    }
+
+    private AnalysisService getAnalysisService() {
+        AnalysisService as = null;
+        Iterator<AnalysisService> it = loader.iterator();
+        for (int i = 0; i <= listY.getSelectedIndex(); i++) {
+            as = it.next();
+        }
+        return as;
     }
 
     private void setLists(String[][] data) {
@@ -98,6 +132,11 @@ public class MainWindow extends JFrame {
         Arrays.stream(data).forEach(strings -> listModelX.addElement(strings[0] + " " + strings[1]));
         loader.forEach(analysisService -> listModelY.addElement(analysisService.getName()));
         pack();
+    }
+
+    private void setListResults(String[][] data) {
+        listModelResults.clear();
+        Arrays.stream(data).forEach(strings -> Arrays.stream(strings).forEach(s -> listModelResults.addElement(s)));
     }
 
     /**
@@ -147,14 +186,20 @@ public class MainWindow extends JFrame {
     }
 
     private void setListsLabels(String[] headers) {
-        label1.setText(headers[0] + " " + headers[1]);
-        label2.setText(headers[1]);
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (int i = 0; i < headers.length - 1; i++) {
+            stringBuilder.append(headers[i]).append(" ");
+        }
+        stringBuilder.append(headers[headers.length - 1]);
+        label1.setText(stringBuilder.toString());
     }
 
     private void createUIComponents() {
         listModelX = new DefaultListModel<>();
         listModelY = new DefaultListModel<>();
+        listModelResults = new DefaultListModel<>();
         listX = new JList<>(listModelX);
         listY = new JList<>(listModelY);
+        list1 = new JList<>(listModelResults);
     }
 }
