63bd829eb761c4ff989a4ecff1fbe9b6d138ccca
danielglazer26 <danielglazer200026@gmail.com>
2022-04-27 11:09:14 +0200

    Add personEndpoint

--- lab08/source/DataBase/src/main/java/pwr/glazer/daniel/database/EventEndpoint.java
+++ lab08/source/DataBase/src/main/java/pwr/glazer/daniel/database/EventEndpoint.java
@@ -2,23 +2,29 @@ package pwr.glazer.daniel.database;
 
 import io.spring.guides.gs_producing_web_service.GetEventRequest;
 import io.spring.guides.gs_producing_web_service.GetEventResponse;
+import io.spring.guides.gs_producing_web_service.GetPersonRequest;
+import io.spring.guides.gs_producing_web_service.GetPersonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import pwr.glazer.daniel.database.model.Event;
+import pwr.glazer.daniel.database.model.Person;
 import pwr.glazer.daniel.database.services.implemantion.EventService;
+import pwr.glazer.daniel.database.services.implemantion.PersonService;
 
 @Endpoint
 public class EventEndpoint {
     private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
     private final EventService eventService;
+    private final PersonService personService;
 
     @Autowired
-    public EventEndpoint(EventService eventService) {
+    public EventEndpoint(EventService eventService, PersonService personService) {
         this.eventService = eventService;
+        this.personService = personService;
     }
 
 
@@ -39,4 +45,24 @@ public class EventEndpoint {
 
         return response;
     }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPersonRequest")
+    @ResponsePayload
+    public GetPersonResponse getPersonResponse(@RequestPayload GetPersonRequest request) {
+        GetPersonResponse response = new GetPersonResponse();
+        int personID = request.getPersonID();
+        Person person = personService.getByPersonID(personID);
+
+        io.spring.guides.gs_producing_web_service.Person p = new io.spring.guides.gs_producing_web_service.Person();
+
+        p.setPersonID(personID);
+        p.setName(person.getName());
+        p.setSurname(person.getSurname());
+
+        response.setPerson(p);
+
+        return response;
+    }
+
+
 }
